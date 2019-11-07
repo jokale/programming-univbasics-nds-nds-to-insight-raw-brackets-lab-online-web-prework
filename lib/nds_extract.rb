0deb8958cb8473b7e004b24f49e1f5df62dcf15c
Joanna E K <kalemajoanna@gmail.com>
2019-11-07 15:34:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,7 +9,14 @@ def directors_totals(nds)
   counter = 0 
   while counter < nds.length do 
      name = nds[counter][:name]
-    total = nds[counter][:movies]
+    total = 0  
+    counter_two = 0 
+    movies = nds[counter][:movies]
+    while counter_two < movies.length do 
+      worldwide_gross = movies[counter_two][:worldwide_gross]
+      total+= worldwide_gross
+      counter_two += 1 
+    end 
   result[name]= total 
     counter += 1 
 end 
